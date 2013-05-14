a096c1fe8b783c871cad72e9ff2753911f25f914
Jon Morehouse <morehousej09@gmail.com>
2013-05-14 02:12:55 -0700

    Update initial application flow / controller flow

---
+++ app/controllers/api/tweet_controller.rb
@@ -0,0 +1,2 @@
+class Api::TweetController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,22 @@ Stacksocial::Application.routes.draw do
 
   match "/logout" => "sessions#destroy", :as => :logout
 
+  # create basic login element for creating new sessions -- this goes through our twitter / auth elements
   match "/login" => "sessions#new", :as => :login 
 
+  # create namespace for api calls etc!
+  # all angular access goes through here
+  namespace :api do
+
+  	# allow for user information updating etc. Grab users and friends etc
+  	resources :user
+
+  	# create / get tweets
+  	resources :tweet
+
+  end
+
+  # basic home page for application
   root :to => 'home#index'
 
 end
