bf68c56c7aee763da7515b1a78be49c63336e554
Jon Morehouse <morehousej09@gmail.com>
2013-05-18 23:55:57 -0700

    Update model / testing structure. Initialize user model

---
+++ app/models/tweet.rb
@@ -0,0 +1,12 @@
+"
+	This is the basic class for storing tweets etc
+	When we initialize a user, we need to grab the tweet for each of the users etc
+	We need to have a class method to grab the tweet, if it doesn't exist then this class is responsible for getting the tweet and saving it etc		
+"
+class Tweet
+  include Mongoid::Document
+
+  	
+
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,44 @@
 "
 	This is the basic twitter user etc
 	We will have concerns / functions that are responsible for updating the elements and then actually grabbing pieces from out twitter code base	
-	
-"
+	We will expect that the initialize element will have at least something like this:
 
+		[uid, credentials: {key, secret}]				
+
+	We're going to store the following information
+
+		[profile name, 
+			key, secret, uid
+			description, 
+			profile pic, 
+			followers,
+			following,
+			tweets,
+		]
+"
 class User
   include Mongoid::Document
 
- 	def initialize() 
+  	# initialize our basic credentials etc
+  	field :uid, type: String
+  	field :key, type: String
+  	field :secret, type: String
 
+  	# now store our basic elements that we want to have accessible via our api	
+  	# this will be a list of all the follower's were currently working with!
+  	# these are all pointers to 
+  	field :followers, type: Array
+  	field :following, type: Array
 
+  	# 
+  	has_many :tweets
+  	# can model these all up later
+  	# has_many :followers
+  	# has_many :following
+
+ 	def initialize(params) 
+
+ 		puts "HELLO WORLD FROM THE USER INITIALIZATION ELEMENT"	
  		
-
  	end
-
-		  	
-
 end
